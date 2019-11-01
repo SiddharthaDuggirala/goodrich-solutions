e2f02553ac1abf09eb3dc81a607fef46cb0b4928
SiddharthaDuggirala <siddharth.dv@gmail.com>
2019-11-01 22:01:49 +0530

    goodrich solutions for chapter 4

---
+++ chapter4/C4-9.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 12:01:09 2019
+
+@author: pcs
+"""
+
+def findMinNumber(l):
+    
+    if len(l) == 0:
+        
+        raise ValueError('Cannot find min value in empty list')
+        
+    elif len(l)==1:
+        return l[0]
+    else:
+        minNumber = findMinNumber(l[1:])
+        minimum = l[0]
+        
+        if minNumber < minimum:
+            minimum = minNumber
+            
+        return minimum
+            
+def findMaxNumber(l):
+    
+    if len(l)==0:
+        raise ValueError('Cannot find max value in empty List')
+        
+    elif len(l)==1:
+        return l[0]
+    
+    else:
+        maxNumber = findMaxNumber(l[1:])
+        maximum = l[0]
+        if maxNumber > maximum:            
+            maximum = maxNumber
+        return maximum
+    
+    
+listA = [9,-2,6,1,80,9,-2]   
+
+print(findMinNumber(listA))
+
+print(findMaxNumber(listA))
